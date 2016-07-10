c6ac1528ddcc6a12ad6fc67b247b64efe0ab242c
zsirkg <zsirkg@yahoo.com>
2016-07-10 15:19:58 +0800

    [macro] modify spell error.

--- macro/symbol_#.c
+++ macro/symbol_#.c
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
     int i1 = 1;
 
     printf("%s\n", str(ssss)); //将ssss转换成字符串并输出。
-    printf("%d\n", cat(i, 1)); //将i和1连接起来，因此出去i1的值。
+    printf("%d\n", cat(i, 1)); //将i和1连接起来，因此打印i1的值。
 
     return 0;
 }
